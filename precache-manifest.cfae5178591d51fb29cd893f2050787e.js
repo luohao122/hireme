2a636635be788e9477975e42d0c8b2db10791dd5
Jake Luong <luong_quoc_hao@sezax.co.jp>
2020-09-08 17:47:35 +0700

    Updates

---
+++ precache-manifest.cfae5178591d51fb29cd893f2050787e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31277d0a02cea508125dde8086109d84",
+    "url": "/hireme/index.html"
+  },
+  {
+    "revision": "5fa8f2a62b664e074791",
+    "url": "/hireme/static/css/main.8e7afd58.chunk.css"
+  },
+  {
+    "revision": "64074df5a4d7df7fc045",
+    "url": "/hireme/static/js/2.99a09813.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hireme/static/js/2.99a09813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa8f2a62b664e074791",
+    "url": "/hireme/static/js/main.cbad5919.chunk.js"
+  },
+  {
+    "revision": "a557628c158a5bc74e0f",
+    "url": "/hireme/static/js/runtime-main.e2341bd4.js"
+  },
+  {
+    "revision": "b4d50e9bea1f864891d8db9657c6637b",
+    "url": "/hireme/static/media/annie.b4d50e9b.png"
+  },
+  {
+    "revision": "69529ea9663a7d6356d7b3f993c15f39",
+    "url": "/hireme/static/media/hyakusen.69529ea9.png"
+  },
+  {
+    "revision": "c2f8967d0e375a8b5978170c48dd4579",
+    "url": "/hireme/static/media/imagestore.c2f8967d.png"
+  },
+  {
+    "revision": "0b5d032e21329a40b7b0fb5e05651150",
+    "url": "/hireme/static/media/jake.0b5d032e.jpg"
+  },
+  {
+    "revision": "ff89e8faafbc2d64d7e063179cf2ffe5",
+    "url": "/hireme/static/media/sezax.ff89e8fa.jpg"
+  },
+  {
+    "revision": "3e917c736c2fbf1655bd8a78ac67abdf",
+    "url": "/hireme/static/media/stayjapan.3e917c73.png"
+  }
+]);
